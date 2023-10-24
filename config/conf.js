abcb9e2a10dbcbf4a662b2e1eece5616fcd81bac
beemarsh <anup8eguy@gmail.com>
2023-10-24 11:30:03 -0500

    Rebuild AWS Amplify

--- config/conf.js
+++ config/conf.js
@@ -1,4 +1,4 @@
-export const APP_TITLE = `Transfer Student Credit\n Management`;
+export const APP_TITLE = `Transfer Student Credit\n Manage`;
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER;
 export const DEBUG = process.env.DEBUG;
 export const NUM_OF_ROWS_PER_PAGE = 10;
